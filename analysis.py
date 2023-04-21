e43eb38bbc5667ad6572e71eff1a4b8cda4da72f
Cjhayy <jameslargo80@yahoo.com>
2023-04-21 19:48:16 +0100

    Refactored code for better readability

--- analysis.py
+++ analysis.py
@@ -19,3 +19,4 @@ Temporary commit content for 2023-03-03T19:48:16
 Temporary commit content for 2023-03-25T19:48:16
 Temporary commit content for 2023-04-13T19:48:16
 Temporary commit content for 2023-04-18T19:48:16
+Temporary commit content for 2023-04-21T19:48:16
