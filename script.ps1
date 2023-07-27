6aa0b579c45309abf0be63515cc7db1584ef6f58
Cjhayy <jameslargo80@yahoo.com>
2023-07-27 19:48:16 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -27,3 +27,4 @@ Temporary commit content for 2023-05-28T19:48:16
 Temporary commit content for 2023-06-09T19:48:16
 Temporary commit content for 2023-06-13T19:48:16
 Temporary commit content for 2023-06-23T19:48:16
+Temporary commit content for 2023-07-27T19:48:16
