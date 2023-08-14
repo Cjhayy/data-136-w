8b203947b31de5745e5322ccec8667aa2d7b9889
Cjhayy <jameslargo80@yahoo.com>
2023-08-14 19:48:16 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -29,3 +29,4 @@ Temporary commit content for 2023-06-13T19:48:16
 Temporary commit content for 2023-06-23T19:48:16
 Temporary commit content for 2023-07-27T19:48:16
 Temporary commit content for 2023-07-30T19:48:16
+Temporary commit content for 2023-08-14T19:48:16
