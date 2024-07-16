affcdbc3e38b81622057d83ca8cdd02ea0f8f485
Cjhayy <jameslargo80@yahoo.com>
2024-07-16 19:48:16 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -57,3 +57,4 @@ Temporary commit content for 2024-04-29T19:48:16
 Temporary commit content for 2024-05-26T19:48:16
 Temporary commit content for 2024-06-03T19:48:16
 Temporary commit content for 2024-06-07T19:48:16
+Temporary commit content for 2024-07-16T19:48:16
