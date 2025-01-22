61d00aebd247537bb1ad3b8760fff77464d74478
Cjhayy <jameslargo80@yahoo.com>
2025-01-22 19:48:16 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -82,3 +82,4 @@ Temporary commit content for 2024-11-18T19:48:16
 Temporary commit content for 2024-12-07T19:48:16
 Temporary commit content for 2024-12-31T19:48:16
 Temporary commit content for 2025-01-20T19:48:16
+Temporary commit content for 2025-01-22T19:48:16
